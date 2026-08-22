132effd247b305a33ad3c703bc1985f43d0d8f6f
Luxolo Mkhathazo <mkhathazol20@gmail.com>
2026-08-22 16:11:11 +0200

    fix(security): correctly decode base64/hex key in JwtService

--- src/main/java/tech/bluespace/security/JwtService.java
+++ src/main/java/tech/bluespace/security/JwtService.java
@@ -2,13 +2,13 @@ package tech.bluespace.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -21,7 +21,7 @@ public class JwtService {
     private long jwtExpiration;
 
     private SecretKey getSigningKey() {
-        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
