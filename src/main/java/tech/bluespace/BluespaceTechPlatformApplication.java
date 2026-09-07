973e751bdbc0f8e3e4cd02dcff709adcec10b0bb
Luxolo Mkhathazo <mkhathazol20@gmail.com>
2026-09-07 23:08:33 +0200

    fix(build): define explicit mainClass in spring-boot-maven-plugin for jar repackaging

---
+++ src/main/java/tech/bluespace/BluespaceTechPlatformApplication.java
@@ -0,0 +1,12 @@
+package tech.bluespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluespaceTechPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluespaceTechPlatformApplication.class, args);
+    }
+}
